5eb078a0e1eba83da12fc812b0873364cf948b58
kaluoci <55224352+kaluoci@users.noreply.github.com>
2025-03-31 23:22:43 +0800

    添加passwall2进入固件编译流程

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -53,6 +53,7 @@ PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 # PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES="$PACKAGES luci-app-passwall2"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
